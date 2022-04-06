9b852fb402037517d05e3671becf7685d7edab35
Nicholas <razvan.botorog@yahoo.com>
2022-04-06 11:17:59 -0400

    FroggerStart

---
+++ app.js
@@ -0,0 +1,6 @@
+// ? Create a grid of 7 x 11 
+// ? Every other row should contain obstacles that are going to move on an interval
+// ? The player starts at the bottom, the goal being to reach the top 
+// ? Different levels, everytime the player reaches the top the interval speed will be inscreased 
+// ? Some obstacles will be still 
+// 
